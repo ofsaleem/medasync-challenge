ed60fd2e97fc09c7b1630145a1e278988cf5d18f
Omar Saleem <ofsaleem@users.noreply.github.com>
2025-04-21 16:25:02 -0400

    adding file opening

--- main.go
+++ main.go
@@ -5,4 +5,17 @@ import (
 	"bufio"
 )
 
+func main() {
+	// must provide filename on command line
+	if len(os.Args) < 2 {
+		fmt.Println("Error: provide filename")
+		os.Exit(1)
+	}
+	// try to open file
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	
 
+}
