b081c72af981291eb532a76dd331b874e0a2c5f9
Omar Saleem <ofsaleem@users.noreply.github.com>
2025-04-23 15:38:55 -0400

    finished e2e test harness. might need to add file cleanup?

--- e2e.sh
+++ e2e.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-for f in $(ls testdata/test*.txt) ; do
-
+for ((i=1;i<=$(ls -1q testdata/test*.txt | wc -l);i++)) ; do
+  f="testdata/test$i.txt"
+  output="testdata/out$i.txt"
+  answers="testdata/answers$i.txt"
+  printf "Processing test $i \n";
+  ./medasync-challenge -f $f > $output
+  if diff $output $answers; then
+    printf "Passed test $i\n"
+  else
+    printf "Failed test $i\n"
+  fi
+done
